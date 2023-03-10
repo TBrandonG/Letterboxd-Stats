b5e276bbb84b0bab76450d542315b37ae5ca3f9d
Brandon Gable <73963728+TBrandonG@users.noreply.github.com>
2023-03-10 10:06:49 -0600

    Update Get-MovieCounts.ps1
    tighten up code, included URL grab to compare like-named movies.

--- Get-MovieCounts.ps1
+++ Get-MovieCounts.ps1
@@ -1,100 +1,94 @@
 cls
 
-$movieList = @()
-$username = read-host "Enter your Letterboxd username as it appears in the URL."
+$username = "b_bombr" #read-host "Enter your Letterboxd username as it appears in the URL."
+
+### Parse your personal Letterboxd Stats page for the available lists and their respective URLs
 $WebResponse = Invoke-WebRequest "https://letterboxd.com/$($username)/stats/"
 $listNames = @(($WebResponse.ParsedHtml.getElementsByTagName('span') | where {$_.classname -eq "list-title yir-label"}).innerHTML.replace("<br>"," ").replace("&amp;","&"))
-$listLinks = @($WebResponse.Links | where{$_.innerHTML -like "*list-progress-inner*"}).href
-$max = $listNames.Count
-if($listLinks.count -gt $listNames.count)
+$listURLs = @($WebResponse.Links | where{$_.innerHTML -like "*list-progress-inner*"}).href
+### Match lists with their respective URLs.
+$listObj = for($i = 0; $i -lt $listNames.Count; $i++)
 {
-    $max = $listLinks.Count
-}
-
-$listObj = for ( $i = 0; $i -lt $max; $i++)
-{
-    Write-Verbose "$($listNames[$i]),$($listLinks[$i])"
+    Write-Verbose "$($listNames[$i]),$($listURLs[$i])"
     [PSCustomObject]@{
         name = $listNames[$i]
-        link = $listLinks[$i]
+        link = "https://letterboxd.com/$($listURLs[$i])"
     }
 }
 
+### Loop through each list and collect movie titles and the respective URLs for each movie to alleviate issues with duplicate titles (ex. Dracula 1931 (/film/dracula/) vs. Dracula 1958 (/film/dracula-1958/))
+$movieTable = @()
 foreach($list in $listObj)
 {
-    $x = 1
+    $page = 1
     $listFound = $true
-    $listMovies = @()
+    $newlistMovies = @()
     while($listFound)
     {
-        Write-Host -ForegroundColor White "Parsing $($list.name), page $($x)"
-        $WebResponse = Invoke-WebRequest "https://letterboxd.com/$($list.link)page/$($x)/"
-        $listMovies += ($WebResponse.images | where {$_.class -eq "Image"}).alt
-        if((($WebResponse.images | where {$_.class -eq "Image"}).alt).count -eq 0)
+        Write-Host -ForegroundColor White "Parsing $($list.name), page $($page)"
+		## Get movie titles if available, if not end while loop, if yes get movie URLS
+        $WebResponse = Invoke-WebRequest "$($list.link)page/$($page)/"
+        $pageMovies = @($WebResponse.images | where {$_.class -eq "Image"}).alt
+        if($pageMovies.count -eq 0)
         {
             $listFound = $false
         }
-        $x ++
-    }
-    $y = 1
-    foreach($movie in $listMovies)
-    {
-        if(!($movie -eq "") -and !($movie -eq $null))
-        {
-            $obj = New-Object psobject -Property @{
-                list = $list.name
-                movie = $movie
-                order = $y
-                url = $list.link
-            }
-            $movieList += $obj
-            $y ++
-         }
+		else
+		{
+			## Get movie URLs and add data to obj with list title, URL, and position in the list
+			$pageURLs = @($WebResponse.ParsedHtml.getElementsByTagName('li') | where {$_.classname -like "poster-container*"}).innerHTML | foreach{$_.Split('""')[9]}
+			for($i = 0; $i -lt $pageMovies.count; $i++)
+			{
+				$obj = New-Object psobject -Property @{
+					list = $list.name
+					name = $pageMovies[$i]
+					movieUrl = "https://letterboxd.com$($pageURLs[$i])"
+					listUrl = $list.link
+                    listPosition = ($page - 1) * 100 + $i + 1
+				}
+				$movieTable += $obj
+			}
+			$page ++
+		}
     }
 }
 
-$x = 1
+### Loop through each page of movies you're watched to collect their titles and URLs
+$page = 1
 $listFound = $true
 $myMovies = @()
 while($listFound)
 {
-    Write-Host -ForegroundColor White "Parsing my watched movies, page $($x)"
-    $WebResponse = Invoke-WebRequest "https://letterboxd.com/$($username)/films/page/$($x)/"
-    $myMovies += ($WebResponse.images | where {$_.class -eq "Image"}).alt
-    if((($WebResponse.images | where {$_.class -eq "Image"}).alt).count -eq 0)
+    Write-Host -ForegroundColor White "Parsing my watched movies, page $($page)"
+    $WebResponse = Invoke-WebRequest "https://letterboxd.com/$($username)/films/page/$($page)/"
+    $pageMovies = @($WebResponse.images | where {$_.class -eq "Image"}).alt
+    if($pageMovies.count -eq 0)
     {
         $listFound = $false
     }
-    $x ++
-}
-$watchedMovies = @()
-foreach($movie in $myMovies)
-{
-    if(!($movie -eq "") -and !($movie -eq $null))
-    {
-        $obj = New-Object psobject -Property @{
-            movie = $movie
-        }
-        $watchedMovies += $obj
-        $y ++
-    }
-}
-
-$listCounts = @()
-foreach($list in $listObj.name)
-{
-    
-     $obj = New-Object psobject -Property @{
-        list = $list
-        count = $movieList.where({$_.list -eq $list}).count
-     }
-     $listCounts += $obj
+	else
+	{
+		$pageURLs = @($WebResponse.ParsedHtml.getElementsByTagName('li') | where {$_.classname -like "poster-container*"}).innerHTML | foreach{$_.Split('""')[9]}
+		for($i = 0; $i -lt $pageMovies.count; $i++)
+		{
+			$obj = New-Object psobject -Property @{
+				name = $pageMovies[$i]
+				URL = "https://letterboxd.com$($pageURLs[$i])"
+			}
+			$myMovies += $obj
+		}
+	}
+    $page ++
 }
 
+## Pull the list of movie titles from IMDB Top 250 page, cause why not, but not reliable in comparing with Letterboxd list due to naming differences ("and" vs. "&" and others)
 $WebResponse = Invoke-WebRequest "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
 $currentIMDB = ($WebResponse.Links.where({$_.innerHTML -eq $_.innerText -and $_.href -match "/title/"})).innerText
 
-#$movieCounts = $movieList.movie | group | sort count -Descending | select name, count
+## Null out old variables
+$null = $pageURLs,$pageMovies,$obj,$WebResponse,$listNames,$listURLs,$listObj,$username
+
+## few reports to start with
 $title = "Reports"
 $message = "Select a report from the options below that you'd like to run."
 $1 = New-Object System.Management.Automation.Host.ChoiceDescription "&1 - View entire table of lists and movies.",`
@@ -109,15 +103,15 @@ $options = [System.Management.Automation.Host.ChoiceDescription[]]($1, $2, $3, $
 $result = $host.ui.PromptForChoice($title,$message,$options,0)
 switch($result) {
     0 {
-        $movieList
+        $movieTable
     }
     1 {
-        $movieList.Where({$_.movie -notin $watchedMovies.movie})
+        $movieTable.Where({$_.movieUrl -notin $myMovies.URL})
     }
     2 {
-        ($movieList.movie | group | sort count -Descending | select name, count).where({$_.Count -gt 3})
+        ($movieTable.movieUrl | group -Property name, movieURL | sort count -Descending | select name, count).where({$_.Count -gt 3})
     }
     3 {
-        (($movieList.Where({$_.movie -notin $watchedMovies.movie})).movie | group | sort count -Descending | select name, count).where({$_.Count -gt 3})
+        ($movieTable.Where({$_.movieUrl -notin $myMovies.URL}) | group -Property name, movieURL | sort count -Descending | select name, count).where({$_.Count -gt 3})
     }
 }
